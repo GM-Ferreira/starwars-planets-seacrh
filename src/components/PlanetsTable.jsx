88056e17f911953c4a400817c936d9c6537d9c55
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-10-20 09:33:52 -0300

    Requisito 3

--- src/components/PlanetsTable.jsx
+++ src/components/PlanetsTable.jsx
@@ -4,47 +4,134 @@ import StarWarsContext from '../context/StarWarsContext';
 
 function PlanetsTable() {
   const [isLoading, setIsLoading] = useState(true);
-  const [colun, setColun] = useState([]);
+  const [column, setColumn] = useState([]);
   const [filteredPlanets, setFilteredPlanets] = useState([]);
-  const [searchPlanet, setSearchPlanet] = useState('');
+  const [searchInput, setSearchInput] = useState('');
+  const [selectedColumn, setSelectedColum] = useState('population');
+  const [selectedOperator, setSelectedOperator] = useState('maior que');
+  const [selectedValue, setSelectedValue] = useState(0);
+  const [objectsFilters, setObjectsFilters] = useState([]);
+
   const { planetList } = useContext(StarWarsContext);
 
+  // didmount puxa requisição
   useEffect(() => {
     setFilteredPlanets(planetList);
     if (planetList.length > 0) {
-      setColun(Object.keys(planetList[0]));
+      setColumn(Object.keys(planetList[0]));
       setIsLoading(false);
     }
   }, [planetList]);
 
-  function handleChange({ target }) {
-    setSearchPlanet(target.value);
-  }
-
+  // atualização de filtros
   useEffect(() => {
-    console.log(searchPlanet);
     const newFiltered = planetList.filter(
-      (planet) => planet.name.includes(searchPlanet),
+      (planet) => planet.name.includes(searchInput),
     );
-    setFilteredPlanets(newFiltered);
-  }, [searchPlanet, planetList]);
+    if (objectsFilters.length > 0 && objectsFilters[0].selectedOperator === 'maior que') {
+      const doubleFilter = newFiltered.filter(
+        (objPlanets) => +(objPlanets[objectsFilters[0].selectedColumn])
+          > +(objectsFilters[0].selectedValue),
+      );
+      console.log('maior que', doubleFilter);
+      setFilteredPlanets(doubleFilter);
+      setSelectedOperator('maior que');
+      setSelectedValue(0);
+      setSelectedColum('population');
+    }
+    if (objectsFilters.length > 0 && objectsFilters[0].selectedOperator === 'menor que') {
+      const doubleFilter = newFiltered.filter(
+        (objPlanets) => +(objPlanets[objectsFilters[0].selectedColumn])
+          < +(objectsFilters[0].selectedValue),
+      );
+      console.log('menor que', doubleFilter);
+      setFilteredPlanets(doubleFilter);
+      setSelectedOperator('maior que');
+      setSelectedValue(0);
+      setSelectedColum('population');
+    }
+    if (objectsFilters.length > 0 && objectsFilters[0].selectedOperator === 'igual a') {
+      const doubleFilter = newFiltered.filter(
+        (objPlanets) => +(objPlanets[objectsFilters[0].selectedColumn])
+          === +(objectsFilters[0].selectedValue),
+      );
+      console.log('igual a ', doubleFilter);
+      setFilteredPlanets(doubleFilter);
+      setSelectedOperator('maior que');
+      setSelectedValue(0);
+      setSelectedColum('population');
+    }
+    if (objectsFilters.length === 0) {
+      setFilteredPlanets(newFiltered);
+    }
+  }, [searchInput, planetList, objectsFilters]);
+
+  // salva novo set de filtros
+  function handleClick() {
+    const filterOption = {
+      selectedColumn, selectedOperator, selectedValue,
+    };
+    setObjectsFilters((prev) => [...prev, filterOption]);
+  }
 
   return (
     <div>
       <p>teste</p>
-      {console.log(filteredPlanets)}
       <input
         type="text"
-        value={ searchPlanet }
-        onChange={ (e) => handleChange(e) }
+        value={ searchInput }
+        onChange={ ({ target }) => setSearchInput(target.value) }
         data-testid="name-filter"
       />
+      <label htmlFor="column-filter">
+        Colun:
+        <select
+          name="column-filter"
+          id="column-filter"
+          data-testid="column-filter"
+          value={ selectedColumn }
+          onChange={ ({ target }) => setSelectedColum(target.value) }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+      </label>
+      <label htmlFor="comparison-filter">
+        Operator:
+        <select
+          name="comparison-filter"
+          id="comparison-filter"
+          data-testid="comparison-filter"
+          value={ selectedOperator }
+          onChange={ ({ target }) => setSelectedOperator(target.value) }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+      </label>
+      <input
+        type="number"
+        data-testid="value-filter"
+        value={ selectedValue }
+        onChange={ ({ target }) => setSelectedValue(target.value) }
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ () => handleClick() }
+      >
+        Filtrar
+      </button>
       { isLoading ? <p>Loading...</p>
         : (
           <table>
             <thead>
               <tr>
-                { colun.map((coluna) => (
+                { column.map((coluna) => (
                   <th key={ coluna }>{ coluna }</th>
                 ))}
               </tr>
